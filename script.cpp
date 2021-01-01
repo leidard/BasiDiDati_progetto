dd299b3838d31b9e5599334377b30acb9565c509
Dario Furlan <dario.furlan.51@gmail.com>
2021-01-01 14:47:45 +0100

    Aggiunto script file

---
+++ script.cpp
@@ -0,0 +1,23 @@
+#include <libpq-fe.h>
+
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+# define PG_HOST "127.0.0.1"
+# define PG_USER " postgres "
+# define PG_DB " filesystem "
+# define PG_PASS " password "
+# define PG_PORT 5432
+
+
+int main(int argc, char** argv) {
+    PGconn* conn = PQconnectdb("dbname");
+
+
+
+    PQfinish(conn);
+    return 0;
+}
